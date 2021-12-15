01108321fe6fdbaa7dd2d7a1dee340a12868e724
marselinusharson <marselinus@undiksha.ac.id>
2021-12-16 07:29:56 +0800

    mencari nilai minimum array  dan nilai serta jenis bilangan pada posisi  sebelumnya

---
+++ Soal_3_2115091030.c
@@ -0,0 +1,51 @@
+/*
+Marselinus Harson Rewo 
+A
+2115091030
+*/
+
+
+#include <stdio.h>
+#include <conio.h>
+
+int main (){
+	printf("=============================Program menampilkan Nilai sebelum nilai minimum=====================\n\n");
+	int n, 
+        min, 
+        i,  
+        posmin, 
+        x ;
+
+	printf("Jumlah elemen array: ");
+	scanf("%d", &n );
+	int arr[n];
+
+	for (i=1 ; i <= n; i++){
+		printf("nilai ke-%d: ", i);	
+		scanf("%d", &arr[i])	;
+	}
+	//procces
+	min = arr[1];
+	posmin = 1;
+	for (i =2; i <=n; i++){
+		if (arr[i] < min){
+		min = arr[i];
+		posmin = i;	
+		}
+	
+	}
+	printf("\nnilai minimum: %d", min);
+		if 	( posmin  == 1){
+			printf("\nNULL\n");
+		} else { 
+			x = arr[posmin - 1];
+			if (x % 2!= 0 ){
+				printf("\nNilai sebelumnya: Ganjil\n");
+			}else {
+				printf("\nNilai sebelumnya: Genap");
+			}
+		}
+getch();
+	return 0;
+
+}
